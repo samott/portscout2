2ea980c9ed39c409653d8d940e6c15f060eee325
Shaun Amott <shaun@inerd.com>
2025-12-12 23:45:16 +0100

    Initial git ports delta implementation.

--- main.go
+++ main.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"log/slog"
-);
+
+	"github.com/samott/portscout2/repo"
+)
 
 func main() {
-	slog.Info("portscout2 running...");
+	slog.Info("portscout2 running...")
+
+	ports := repo.FindUpdated("/usr/ports", "b700f9a18a81834a7e5c2046cda87c290bfa229a")
+
+	slog.Info("Ports", "ports", ports)
 }
