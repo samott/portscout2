ba5d07938f3061c4e885669992b48856562b88da
Shaun Amott <shaun@inerd.com>
2026-01-07 20:29:35 +0100

    Add tests for type marshaling.

---
+++ types/types_test.go
@@ -0,0 +1,73 @@
+package types
+
+import (
+	"testing"
+)
+
+func TestUnmarshalTaggedLists(t *testing.T) {
+	result := UnmarshalTaggedLists("http://x.com/:tag1,tag2 http://y.com/:tag1 http://z.com/:tag3 http://q.com")
+
+	if len(result) != 4 {
+		t.Fatal("Incorrect tag count")
+	}
+
+	if _, ok := result[""]; !ok {
+		t.Fatal("Missing tag: default tag")
+	}
+
+	if _, ok := result["tag1"]; !ok {
+		t.Fatal("Missing tag: tag1")
+	}
+
+	if _, ok := result["tag2"]; !ok {
+		t.Fatal("Missing tag: tag2")
+	}
+
+	if _, ok := result["tag3"]; !ok {
+		t.Fatal("Missing tag: tag3")
+	}
+
+	if len(result["tag1"].items) != 2 {
+		t.Fatal("Incorrect item count for tag1")
+	}
+
+	if len(result["tag2"].items) != 1 {
+		t.Fatal("Incorrect item count for tag2")
+	}
+
+	if len(result["tag3"].items) != 1 {
+		t.Fatal("Incorrect item count for tag3")
+	}
+}
+
+func TestMarshalTaggedLists(t *testing.T) {
+	input := map[string]*TaggedList{
+		"tag1": &TaggedList{
+			items: []string{
+				"http://www.example.net",
+				"http://www.example.com",
+			},
+		},
+		"tag2": &TaggedList{
+			items: []string{
+				"http://www.example.org",
+			},
+		},
+		"tag3": &TaggedList{
+			items: []string{
+				"http://www.example.org",
+			},
+		},
+		"": &TaggedList{
+			items: []string{
+				"http://www.microsoft.com",
+			},
+		},
+	}
+
+	output := MarshalTaggedLists(input)
+
+	if output != "http://www.microsoft.com http://www.example.net:tag1 http://www.example.com:tag1 http://www.example.org:tag2 http://www.example.org:tag3" {
+		t.Fatal("Incorrect marshaled tagged lists string:", output)
+	}
+}
