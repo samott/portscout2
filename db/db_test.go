7692275137c681a18ba8546b21e39fdfb7bc1d74
Shaun Amott <shaun@inerd.com>
2026-06-21 01:34:36 +0200

    Add initial tests for DB module.

---
+++ db/db_test.go
@@ -0,0 +1,101 @@
+package db
+
+import (
+	"flag"
+	"log/slog"
+	"os"
+	"testing"
+
+	"github.com/samott/portscout2/config"
+	"github.com/samott/portscout2/types"
+)
+
+var db *DB
+
+func TestMain(m *testing.M) {
+	configFile := flag.String("config", "../portscout.test.yaml", "path to configuration file")
+
+	flag.Parse()
+
+	cfg, err := config.LoadConfig(*configFile)
+
+	if err != nil {
+		slog.Error("Failed to load config file " + *configFile)
+		os.Exit(1)
+	}
+
+	db, err = NewDB(cfg.Db.Url)
+
+	code := m.Run()
+
+	os.Exit(code)
+}
+
+func TestUpdatePort(t *testing.T) {
+	err := db.RemovePort(types.PortName{
+		Name:     "test",
+		Category: "cat",
+	})
+
+	if err != nil {
+		t.Fatal("RemovePort failed")
+	}
+
+	err = db.UpdatePort(types.PortInfo{
+		Name: types.PortName{
+			Name:     "test",
+			Category: "cat",
+		},
+		Maintainer: "test@example.net",
+	})
+
+	if err != nil {
+		t.Fatal("UpdatePort failed")
+	}
+
+	port, err := db.GetPortByName(types.PortName{
+		Name:     "test",
+		Category: "cat",
+	})
+
+	if err != nil {
+		t.Fatal("GetPortByName failed")
+	}
+
+	if port == nil {
+		t.Fatal("Port not found")
+	}
+
+	if port.Name.Name != "test" || port.Name.Category != "cat" || port.Maintainer != "test@example.net" {
+		t.Fatal("Incorrect field value")
+	}
+
+	err = db.UpdatePort(types.PortInfo{
+		Name: types.PortName{
+			Name:     "test",
+			Category: "cat",
+		},
+		Maintainer: "newmaintainer@example.net",
+	})
+
+	if err != nil {
+		t.Fatal("UpdatePort failed")
+	}
+
+	port, err = db.GetPortByName(types.PortName{
+		Name:     "test",
+		Category: "cat",
+	})
+
+	if err != nil {
+		t.Fatal("GetPortByName failed")
+	}
+
+	if port == nil {
+		t.Fatal("Port not found")
+	}
+
+	if port.Name.Name != "test" || port.Name.Category != "cat" || port.Maintainer != "newmaintainer@example.net" {
+		t.Fatal("Incorrect field value")
+	}
+}
